15e356ab9b26ab73ac6f7e21101b9cb6da137aa9
FabricioCCapone <fabricapone12@gmail.com>
2025-01-26 00:56:25 +0100

    navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ export const Navbar = () => {
             </div>
             <div className={styles.divul}>
                 <ul className={styles.ul}>
-                    <li><a className={styles.a} href="#about"><a>01.</a>About</a></li>
+                    <li><a className={styles.a} href="#about"><a>01.</a>About    </a></li>
                     <li><a className={styles.a} href="#projects"><a>02.</a>Projects</a></li>
                     <li><a className={styles.a} href="/CV_CaponeFabricio.pdf" target='_blank' ><a>03.</a>Resume</a></li>
-                    <li><a className={styles.a} style={{ marginRight: '0px' }} href="#contact"><a>04.</a>Contact</a></li>
+                    <li><a className={styles.a}  href="#contact"><a>04.</a>Contact</a></li>
                 </ul>
             </div>
         </nav>
